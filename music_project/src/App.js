0aca3daebb4599771395d3308c799c1bb1cdf893
Allyson Walpole <zibbycakes@gmail.com>
2018-08-25 22:41:46 -0500

    added reactstrap for table formatting. Data outputted as table. Think I'm done.

--- music_project/src/App.js
+++ music_project/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MusicFormatter from './MusicFormatter'
+import {Table} from 'reactstrap'
 
 class App extends Component {
   constructor()
@@ -9,18 +10,50 @@ class App extends Component {
     super();
     this.mf = new MusicFormatter();
   }
-  render() {
+
+  convertToTable() {
     this.mf.formatDB();
     console.log(this.mf.clean_db);
+
+    this.formattedTableArray = [];
+    for(let entry of this.mf.clean_db)
+    {
+      this.formattedTableArray.push(
+        <tr key={entry.index}>
+          <th scope="row">{entry.index}</th>
+          <td><img src={entry.album_thumbnail}/></td>
+          <td>{entry.track_title}</td>
+          <td>{entry.artist_names}</td>
+          <td>{entry.album_title}</td>
+          <td>{entry.year}</td>
+          <td>{entry.track_no}</td>
+          <td>{entry.total_track}</td>
+        </tr>
+      )
+    }
+  }
+
+  render() {
+    this.convertToTable();
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Table hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Art</th>
+            <th>Track Name</th>
+            <th>Artist Names</th>
+            <th>Album Name</th>
+            <th>Year released</th>
+            <th>Track #</th>
+            <th>Total tracks on album</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.formattedTableArray}
+        </tbody>
+      </Table>
       </div>
     );
   }
--- music_project/src/MusicFormatter.js
+++ music_project/src/MusicFormatter.js
@@ -13,16 +13,17 @@ class MusicFormatter {
 
     formatDB()
     {
-        var clean_entry = {track_title:'',album_title:'',album_thumbnail:'',artist_names:'',year:'',total_track:'',track_no:''};
+        var index =0;
+        var clean_entry = {index:0, track_title:'',album_title:'',album_thumbnail:'',artist_names:'',year:'',total_track:'',track_no:''};
         for(let entry of db.items) {
             //console.log(track);
-            clean_entry = {track_title:'',album_title:'',album_thumbnail:'',artist_names:'',year:'',total_track:'',track_no:''};
+            clean_entry = {index, track_title:'',album_title:'',album_thumbnail:'',artist_names:'',year:'',total_track:'',track_no:''};
             clean_entry.track_title = entry.track.name;
             clean_entry.track_no = entry.track.track_number;
             clean_entry.album_title = entry.track.album.name;
             clean_entry.total_track = entry.track.album.total_tracks;
 
-            clean_entry.album_thumbnail = entry.track.album.images[0].url;
+            clean_entry.album_thumbnail = entry.track.album.images[1].url;
 
             clean_entry.year = entry.track.album.release_date.substring(0,4);
 
@@ -34,7 +35,7 @@ class MusicFormatter {
             clean_entry.artist_names = temp_artists.substring(0, temp_artists.length-2);
 
             this.clean_db.push(clean_entry);
-
+            index++;
         }
     }
 }
