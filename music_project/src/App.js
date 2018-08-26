8df372ae24eb73a4ae84daed8a947d9e61483f2d
Allyson Walpole <zibbycakes@gmail.com>
2018-08-25 22:09:56 -0500

    music formatter formats correctly. Just need to display

--- music_project/src/App.js
+++ music_project/src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MusicFormatter from './MusicFormatter'
 
 class App extends Component {
+  constructor()
+  {
+    super();
+    this.mf = new MusicFormatter();
+  }
   render() {
+    this.mf.formatDB();
+    console.log(this.mf.clean_db);
     return (
       <div className="App">
         <header className="App-header">
---
+++ music_project/src/MusicFormatter.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import db from './spotify_api_results.json';
+
+class MusicFormatter {
+    constructor(){
+        //super();
+        this.i = 0;
+        this.first = db.items[0];
+        this.clean_db = [];
+    }
+
+    formatDB()
+    {
+        var clean_entry = {track_title:'',album_title:'',album_thumbnail:'',artist_names:'',year:'',total_track:'',track_no:''};
+        for(let entry of db.items) {
+            //console.log(track);
+            clean_entry = {track_title:'',album_title:'',album_thumbnail:'',artist_names:'',year:'',total_track:'',track_no:''};
+            clean_entry.track_title = entry.track.name;
+            clean_entry.track_no = entry.track.track_number;
+            clean_entry.album_title = entry.track.album.name;
+            clean_entry.total_track = entry.track.album.total_tracks;
+
+            clean_entry.album_thumbnail = entry.track.album.images[0].url;
+
+            clean_entry.year = entry.track.album.release_date.substring(0,4);
+
+            var temp_artists = "";
+            for(let artist of entry.track.artists)
+            {
+                temp_artists += artist.name + ", ";
+            }
+            clean_entry.artist_names = temp_artists.substring(0, temp_artists.length-2);
+
+            this.clean_db.push(clean_entry);
+
+        }
+    }
+}
+export default MusicFormatter;
